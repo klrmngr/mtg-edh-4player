169c338958b94b93011e86d2054801685a0a586e
Kai <kaimingzhang1234@gmail.com>
2026-06-25 13:56:49 -0500

    chore: bump VERSION to v0.1.5

--- main.lua
+++ main.lua
@@ -4206,7 +4206,7 @@ end
 
 -------------------------------- PATCH NOTES -----------------------------------
 -- Version currently deployed. Bump this when cutting a new release.
-VERSION = "v0.1.4"
+VERSION = "v0.1.5"
 
 local PATCH_NOTES_TAG = "patchNotesButton"
 local RELEASES_API = "https://api.github.com/repos/klrmngr/mtg-edh-4player/releases"
